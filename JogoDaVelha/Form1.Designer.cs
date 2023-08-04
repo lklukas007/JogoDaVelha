08fd91e0ec263e9cc79cf2ee7fd0d4348d52dd4a
Your Name <lklukas@outlook.com>
2023-08-03 22:23:29 -0300

    Add project files.

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button00 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button00
+            // 
+            this.button00.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button00.Location = new System.Drawing.Point(66, 12);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(142, 143);
+            this.button00.TabIndex = 0;
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button01
+            // 
+            this.button01.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button01.Location = new System.Drawing.Point(230, 12);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(142, 143);
+            this.button01.TabIndex = 1;
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button02
+            // 
+            this.button02.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button02.Location = new System.Drawing.Point(392, 12);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(142, 143);
+            this.button02.TabIndex = 2;
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(66, 161);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(142, 143);
+            this.button10.TabIndex = 3;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(230, 161);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(142, 143);
+            this.button11.TabIndex = 4;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(392, 161);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(142, 143);
+            this.button12.TabIndex = 5;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button22.Location = new System.Drawing.Point(392, 310);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(142, 143);
+            this.button22.TabIndex = 6;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button21.Location = new System.Drawing.Point(230, 310);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(142, 143);
+            this.button21.TabIndex = 7;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button20
+            // 
+            this.button20.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button20.Location = new System.Drawing.Point(66, 310);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(142, 143);
+            this.button20.TabIndex = 8;
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 470);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button00);
+            this.Name = "Form1";
+            this.Text = "Jogo Da Velha";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button20;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        private char[,] board = new char[3, 3];
+        private char currentPlayer = 'X';
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+            // Percorre todos os botões e redefine o texto para vazio
+            foreach (Control control in Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Text = "";
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int row = int.Parse(clickedButton.Name[6].ToString());
+            int col = int.Parse(clickedButton.Name[7].ToString());
+
+            if (board[row, col] == ' ')
+            {
+                board[row, col] = currentPlayer;
+                clickedButton.Text = currentPlayer.ToString();
+
+                if (CheckWinner(currentPlayer))
+                {
+                    MessageBox.Show($"O jogador {currentPlayer} venceu!");
+                    InitializeBoard();
+
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("Empate! O jogo terminou sem vencedor.");
+                    InitializeBoard();
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+
+        private bool CheckWinner(char player)
+        {
+            // Lógica para verificar se há um vencedor (linhas, colunas, diagonais)
+            // Verificação de linhas
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == player && board[row, 1] == player && board[row, 2] == player)
+                {
+                    return true; // Jogador venceu na linha 'row'
+                }
+            }
+
+            // Verificação de colunas
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == player && board[1, col] == player && board[2, col] == player)
+                {
+                    return true; // Jogador venceu na coluna 'col'
+                }
+            }
+
+            // Verificação de diagonais
+            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+            {
+                return true; // Jogador venceu na diagonal principal
+            }
+            if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+            {
+                return true; // Jogador venceu na diagonal secundária
+            }
+
+            return false; // Nenhum jogador venceu
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (board[row, col] == ' ')
+                    {
+                        return false; // Ainda há espaços vazios
+                    }
+                }
+            }
+            return true; // Todos os campos estão preenchidos
+        }
+
+    }
+}
